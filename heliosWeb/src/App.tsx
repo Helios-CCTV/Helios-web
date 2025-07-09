1b1c4e5575fb4d5c7237ea247150d408fa70bbcf
sehyun lee <mh080411@kyonggi.ac.kr>
2025-07-09 19:58:55 +0900

    create new project

---
+++ heliosWeb/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+
+  return (
+    <>
+      <div className='flex justify-center justify-items-center'>
+        <div className="text-blue-600 dark:text-sky-400 underline text-7xl flex justify-center items-center">
+          Hello world!
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
