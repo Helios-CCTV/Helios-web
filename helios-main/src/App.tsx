c609df559d98ec916ae132f8c57d60e8358414dd
sehyun lee <mh080411@kyonggi.ac.kr>
2025-07-12 18:08:06 +0900

    vite + react + typescript + tailwindcss project create

---
+++ helios-main/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="bg-red-200 m-25">
+        <div className="font-bold">hello world</div>
+      </div>
+    </>
+  )
+}
+
+export default App
