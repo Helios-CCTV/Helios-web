07f0bc033ccd6469982ae92d4703725f8a9e829d
sehyun lee <mh080411@kyonggi.ac.kr>
2025-07-14 13:07:34 +0900

    slogan 지정, font 버그 수정

--- helios-main/src/components/MainPage/Slogan.tsx
+++ helios-main/src/components/MainPage/Slogan.tsx
@@ -6,8 +6,8 @@ export default function Slogan() {
     <>
     <div className="w-screen h-screen flex items-start justify-center relative">
       <img className="min-w-[1280px] min-h-[720px] w-[100vw] h-[90vh] " src="src/assets/backgroundImage.png" alt="slogan" />
-      <div className='absolute justify-center items-center top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-[700] text-[60px]'>AI가 먼저 알고 즉시 알려요.
-      <div className='transform translate-x-4'>더 안전한 길 우리와 함께</div></div>
+      <div className='absolute flex justify-center items-center top-1/3 transform -translate-y-1/2 font-[700] text-[60px]'>AI가 먼저 알고 즉시 알려요.</div>
+      <div className='absolute flex items-center justify-center top-[40%] transform  -translate-y-1/2 font-[700] text-[60px]'>더 안전한 길 우리와 함께</div>
     </div>
     </>
   )
