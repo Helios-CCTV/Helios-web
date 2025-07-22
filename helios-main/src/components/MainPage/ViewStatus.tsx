6bba99716204e352e56ca389991394105bb6b5ec
sehyun lee <mh080411@kyonggi.ac.kr>
2025-07-23 06:24:12 +0900

    메인 페이지 구조 개선 및 ViewStatus 컴포넌트 추가

---
+++ helios-main/src/components/MainPage/ViewStatus.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function ViewStatus() {
+  return (
+    <div className='w-full h-screen'>
+        <div className=''>CCTV 현황 보기</div>
+
+        <div>전국 CCTV 현황</div>
+        <div>한번에</div>
+        <div>조회하기</div>
+    </div>
+  )
+}
--- helios-main/src/pages/MainPage/MainPage.tsx
+++ helios-main/src/pages/MainPage/MainPage.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Banner from '../../components/Banner/Banner'
 import Slogan from '../../components/MainPage/Slogan'
+import ViewStatus from '../../components/MainPage/ViewStatus'
 
 export default function MainPage() {
   return (
     <>
-        <Banner/>
-        <Slogan/>
+      <div className='snap-y snap-mandatory h-screen overflow-y-scroll'>
+        <div><Banner/></div>
+        <div className='snap-start w-full h-screen'><Slogan/></div>
+        <div className='snap-start w-full h-screen'><ViewStatus/></div>
+      </div>      
     </>
   )
 }
