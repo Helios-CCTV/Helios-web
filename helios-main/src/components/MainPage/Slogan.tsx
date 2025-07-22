828b2b5f90ebb03146970e610eea9e9eecc3b43d
sehyun lee <mh080411@kyonggi.ac.kr>
2025-07-22 12:22:50 +0900

    슬로건 컴포넌트 개선 및 배경 그라데이션 적용

--- helios-main/src/components/MainPage/Slogan.tsx
+++ helios-main/src/components/MainPage/Slogan.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 
-// 배너 작업 중, 이미지 그라데이션 작업 해야함
+// 슬로건, 메인 배너 이미지에 대한 컴포넌트
 export default function Slogan() {
   return (
     <>
-    {/* 전체 div 그라데이션 작업 중이였음 */} 
-    <div className="w-screen h-screen flex items-start justify-center relative bg-gradient-to-t from-white to-*">
+    {/* 전체 div */}
+    <div className="w-screen h-screen flex items-start justify-center relative">
+      {/* gradation 적용 */}
+      <div className='fixed bg-gradient-to-b from-white to-white/0 z-50 h-[10%] w-full top-[7.3vh]'></div>
       {/* 로고 */}
-      <img className="w-full h-[99vh] object-cover" src="src/assets/backgroundImage.png" alt="slogan" />
+      <img className="fixed w-full h-[99vh] mt-[7.3vh] object-cover" src="src/assets/backgroundImage.png" alt="slogan" />
       
+
+
       {/* 슬로건 */}
       <div className='absolute flex justify-center items-center transform -translate-y-1/2 top-1/3 text-[30px] lg:top-1/3 font-[700] lg:text-[60px] md:text-[50px] sm:text-[40px]'>AI가 먼저 알고 즉시 알려요.</div>
       <div className='absolute flex justify-center items-center transform -translate-y-1/2 top-[38%] text-[30px] lg:top-[45%] md:top-[43%] sm:top-[40%] font-[700] lg:text-[60px] md:text-[50px] sm:text-[40px]'>더 안전한 길 우리와 함께</div>
