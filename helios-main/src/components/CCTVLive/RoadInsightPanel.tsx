c6747525bf6373ab423e181592139231a5f00886
sehyun lee <mh080411@kyonggi.ac.kr>
2025-07-24 18:30:15 +0900

    CCTVLive 페이지에 MapPage, RoadInsightPanel 컴포넌트 추가

---
+++ helios-main/src/components/CCTVLive/RoadInsightPanel.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+export default function RoadInsightPanel() {
+  return (
+    <>
+      <div className='w-[90px] h-[10px] z-50 font-[700] text-[100px] absolute mt-[100px] bg-amber-950'></div>
+    </>
+  )
+}
--- helios-main/src/pages/CCTVLive/LivePage.tsx
+++ helios-main/src/pages/CCTVLive/LivePage.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Banner from '../../components/Banner/Banner'
-import MapPage from './MapPage'
+import MapPage from '../../components/CCTVLive/MapPage'
+import RoadInsightPanel from '../../components/CCTVLive/RoadInsightPanel'
+
 
 export default function LivePage() {
   return (
     <>
       <Banner />
       <MapPage />
+      <RoadInsightPanel />
     </>
   )
 }
