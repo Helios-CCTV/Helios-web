1c769fd551c2e94b2089153ce94072e9939f2da0
sehyun lee <mh080411@kyonggi.ac.kr>
2025-07-31 16:33:45 +0900

    RoadInsightPanel 퍼블리싱 진행, 기능적 요소 X

--- helios-main/src/components/CCTVLive/RoadInsightPanel.tsx
+++ helios-main/src/components/CCTVLive/RoadInsightPanel.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 export default function RoadInsightPanel() {
   return (
     <>
-      <div className='flex w-[24vw] h-[56vh] z-50 absolute mt-[124px] ml-[24px] bg-zinc-50 justify-center rounded-xl shadow-lg'>
+      <div className='flex w-[22vw] h-auto z-50 absolute mt-[79px] pb-[24px] ml-[24px] bg-zinc-50 justify-center rounded-xl shadow-lg'>
         <div className='flex flex-col items-center w-full px-4'>
           <div className='relative flex items-center w-full h-[4.8vh] mt-[24px]'>
             <input
@@ -13,14 +13,32 @@ export default function RoadInsightPanel() {
               placeholder='주소를 입력해주세요.'
             />
             <button className='absolute right-3 top-1/2 transform -translate-y-1/2' onClick={() => alert('검색 기능은 아직 구현되지 않았습니다.')}>
-              <img className='w-[1.5vw] h-[1.5vw]' src="src/assets/livePage/glasses.png" alt="검색" />
+              <img className='w-[14px] h-[17px]' src="src/assets/livePage/glasses.png" alt="검색" />
             </button>
           </div>
 
-          <div className='flex justify-between mt-4 w-full'>
-            <div className='border rounded-full px-4 py-1 border-black'>위험</div>
-            <div className='border rounded-full px-4 py-1 border-black'>주의</div>
-            <div className='border rounded-full px-4 py-1 border-black'>안전</div>
+          <div className='flex gap-[25px] mt-4 w-full justify-center'>
+            <div className='flex flex-row items-center justify-center w-auto h-[35px] px-[16px] py-[6px] border rounded-full gap-[6px] border-black'>
+              <img className='w-[15px] h-[15px]' src="src/assets/livePage/danger.png" alt="위험" />
+              <div className='font-[700] text-[12px]'>
+                위험
+              </div>
+            </div>
+
+            <div className='flex flex-row items-center justify-center w-auto h-[35px] px-[16px] py-[6px] border rounded-full gap-[6px] border-black'>
+              <img className='w-[15px] h-[15px]' src="src/assets/livePage/warning.png" alt="주의" />
+              <div className='font-[700] text-[12px]'>
+                주의
+              </div>
+            </div>
+
+            <div className='flex flex-row items-center justify-center w-auto h-[35px] px-[16px] py-[6px] border rounded-full gap-[6px] border-black'>
+              <img className='w-[15px] h-[15px]' src="src/assets/livePage/normal.png" alt="안전" />
+              <div className='font-[700] text-[12px]'>
+                안전
+              </div>
+            </div>
+
           </div>
 
           <div className='bg-white border rounded-xl border-neutral-300 mt-6 p-4 w-full'>
